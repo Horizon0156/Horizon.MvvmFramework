ec39806e843cabff6b59efb82a3b4d30fe4d151f
Stefan Wilkes <stefan.wilkes@gmail.com>
2016-12-26 12:28:44 +0100

    Added Unit Test project and Unit Tests for the InvertedBooleanConverter

---
+++ Horizon.MvvmFramework.Wpf.Tests/Converter/InvertedBooleanConverterTests.cs
@@ -0,0 +1,45 @@
+﻿using Horizon.MvvmFramework.Wpf.Converter;
+using System.Globalization;
+using System.Windows;
+using Xunit;
+
+namespace Horizon.MvvmFramework.Wpf.Tests.Converter
+{
+    public class InvertedBooleanConverterTests
+    {
+        private InvertedBooleanConverter _testSubject;
+
+        public InvertedBooleanConverterTests()
+        {
+            _testSubject = new InvertedBooleanConverter();
+        }
+
+        [Theory]
+        [InlineData(false, true)]
+        [InlineData(true, false)]
+        public void TestConvert(object inputValue, object expectedResult)
+        {
+            var result = _testSubject.Convert(inputValue, typeof(bool), null, CultureInfo.CurrentCulture);
+
+            Assert.Equal(result, expectedResult);
+        }
+
+        [Theory]
+        [InlineData(false, true)]
+        [InlineData(true, false)]
+        public void TestConvertBack(object inputValue, object expectedResult)
+        {
+            var result = _testSubject.ConvertBack(inputValue, typeof(bool), null, CultureInfo.CurrentCulture);
+
+            Assert.Equal(result, expectedResult);
+        }
+
+        [Fact]
+        public void TestConvertingNonBooleanValuesShouldReturnUnsetProperties()
+        {
+            var result = _testSubject.Convert(42, typeof(bool), null, CultureInfo.CurrentCulture);
+
+            Assert.Equal(result, DependencyProperty.UnsetValue);
+        }
+    }
+}
